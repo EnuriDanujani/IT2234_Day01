2228dd872b5653173eda70f41ee62ff446ec3f5d
EnuriDanujani <enuridanujani2000@gmail.com>
2025-03-24 07:38:07 +0530

    prime

---
+++ prime.js
@@ -0,0 +1,13 @@
+ //write a boolean function to find the given number is prime
+function prime(num)
+{
+	 if(num == 0 && num == 1)
+	 {
+		 return("this is not a prime number")
+	 }
+	 else
+	 {
+		 return("This is not a prime number")
+	 }
+ }
+console.log(prime(15))
